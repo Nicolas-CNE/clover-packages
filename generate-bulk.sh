96bee0963a1784f10c7d0cbe5863c4762b7499ca
Nicolas-CNE <Nicolas-CNE@users.noreply.github.com>
2026-05-23 18:22:16 +0000

    Fix: cowsay deleted

--- generate-bulk.sh
+++ generate-bulk.sh
@@ -18,7 +18,6 @@ EOF
 # --- ADDED PACKAGES ---
 create_recipe "neofetch" "https://github.com/dylanaraps/neofetch/archive/refs/tags/7.1.0.tar.gz" "script"
 create_recipe "cmatrix" "https://github.com/abishekvashok/cmatrix/archive/refs/heads/master.tar.gz" "c"
-create_recipe "cowsay" "https://github.com/takinami/cowsay/archive/refs/heads/master.tar.gz" "script"
 # ---------------------------------------------
 create_recipe "nano" "https://github.com/nano-editor/nano/archive/refs/tags/v8.0.tar.gz" "c"
 create_recipe "htop" "https://github.com/htop-dev/htop/archive/refs/tags/3.3.0.tar.gz" "c"
