ad121dc60cb2c7ce2fe00f21ce8d789b0be1415b
Nicolas-CNE <255580905+Nicolas-CNE@users.noreply.github.com.>
2026-09-09 19:32:36 -0300

    refactor: remove fortune core source code, keeping repository exclusively for recipes

--- network.h
+++
@@ -1,9 +0,0 @@
-#ifndef FORTUNE_NETWORK_H
-#define FORTUNE_NETWORK_H
-
-#include <curl/curl.h>
-
-int net_download_file(const char *url, const char *output_path);
-char *net_fetch_string(const char *url);
-
-#endif
--- sync.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYNC_H
-#define SYNC_H
-
-#define RECIPES_REPO_URL "https://github.com/Nicolas-CNE/clover-packages/archive/refs/heads/main.tar.gz"
-#define RECIPES_DIR "/var/lib/fortune/recipes"
-
-int fortune_sync(void);
-
-#endif // SYNC_H
