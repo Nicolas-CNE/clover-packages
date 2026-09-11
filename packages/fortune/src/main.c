e0cdae2470f00dd8591ea0715ed35fda5b833445
Nick-Clover. <255580905+Nicolas-CNE@users.noreply.github.com>
2026-09-11 12:49:29 -0300

    Refactor command handling in main.c

--- packages/fortune/src/main.c
+++ packages/fortune/src/main.c
@@ -9,13 +9,13 @@ void print_usage(const char *prog_name) {
     printf("Usage: %s [options] <command> [<package>]\n\n", prog_name);
     printf("Commands:\n");
     printf("  sync                Sync remote recipe repositories\n");
-    printf("  install <package>   Install a software package\n");
+    printf("  search <query>      Search packages in local index\n");
+    printf("  install <package>   Build and install a package and its dependencies\n");
     printf("  uninstall <package> Remove an installed package\n");
-    printf("  list                List installed packages\n");
-    printf("  pkg-build <package> Build standalone package binary\n\n");
+    printf("  list                List installed packages\n\n");
     printf("Options:\n");
-    printf("  -v, --verbose        Enable detailed logging\n");
-    printf("  -h, --help           Show this help\n");
+    printf("  -v, --verbose       Enable detailed logging\n");
+    printf("  -h, --help          Show this help\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -24,25 +24,42 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    // Detectar flag verbose (-v o --verbose)
+    int verbose = 0;
+    int arg_offset = 1;
+
+    if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0) {
+        verbose = 1;
+        arg_offset = 2;
+        if (argc < 3) {
+            print_usage(argv[0]);
+            return 1;
+        }
+    }
+
+    const char *cmd = argv[arg_offset];
+
+    // Adquirir cerrojo de base de datos
     int lock_fd = db_lock();
     if (lock_fd < 0) {
         return 1;
     }
 
-    if (strcmp(argv[1], "sync") == 0) {
+    if (strcmp(cmd, "sync") == 0) {
         fortune_sync();
-    } else if (strcmp(argv[1], "install") == 0 && argc >= 3) {
-        core_install(argv[2]);
-    } else if (strcmp(argv[1], "uninstall") == 0 && argc >= 3) {
-        core_uninstall(argv[2]);
-    } else if (strcmp(argv[1], "list") == 0) {
+    } else if (strcmp(cmd, "search") == 0 && argc >= (arg_offset + 2)) {
+        cmd_search(argv[arg_offset + 1]);
+    } else if (strcmp(cmd, "install") == 0 && argc >= (arg_offset + 2)) {
+        core_install(argv[arg_offset + 1], verbose);
+    } else if (strcmp(cmd, "uninstall") == 0 && argc >= (arg_offset + 2)) {
+        core_uninstall(argv[arg_offset + 1]);
+    } else if (strcmp(cmd, "list") == 0) {
         core_list_installed();
-    } else if (strcmp(argv[1], "pkg-build") == 0 && argc >= 3) {
-        core_pkg_build(argv[2]);
     } else {
         print_usage(argv[0]);
     }
 
+    // Liberar cerrojo de base de datos
     db_unlock(lock_fd);
     return 0;
 }
