2fed0392badecf3b703ad8e992013baa86ff724f
Nick-Clover. <255580905+Nicolas-CNE@users.noreply.github.com>
2026-09-11 13:03:58 -0300

    update core.h with new core install verbose thingy

--- packages/fortune/src/core.h
+++ packages/fortune/src/core.h
@@ -1,10 +1,8 @@
 #ifndef FORTUNE_CORE_H
 #define FORTUNE_CORE_H
 
-int core_pkg_build(const char *pkg_name);
 int core_verify_privileges(void);
-int core_install(const char *pkg_name, int verbose);
-void core_install_bin(const char *pkg_name);
+void core_install(const char *pkg_name, int verbose);
 void core_uninstall(const char *pkg_name);
 void core_list_installed(void);
 int core_sync_repo(void);
