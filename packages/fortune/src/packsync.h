4356af75b7dd0b634ed665ea7754f69d5924d217
Nick-Clover. <255580905+Nicolas-CNE@users.noreply.github.com>
2026-09-11 12:35:54 -0300

    implementing index babyyyyyyyy

--- packages/fortune/src/packsync.h
+++ packages/fortune/src/packsync.h
@@ -1,7 +1,10 @@
 #ifndef PACKSYNC_H
 #define PACKSYNC_H
 
-// Sincroniza las recetas filtrando solo la carpeta packages/
+// Descarga únicamente el archivo PACKINDEX.txt a /var/lib/fortune/recipes
 int fortune_sync_repository(const char *repo_url, const char *target_dir);
+
+// Descarga la receta de un paquete específico si no existe localmente
+int fetch_recipe_if_missing(const char *pkg_name, const char *target_dir);
 
 #endif
