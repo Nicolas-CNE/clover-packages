4c86bd61b0dc11b1e579a85bdfbfa0b4428977a7
Nick-Clover. <255580905+Nicolas-CNE@users.noreply.github.com>
2026-09-11 12:21:00 -0300

    Create packsync.h for fortune sync functionality
    so you dont download all of the fucking repo

---
+++ packages/fortune/src/packsync.h
@@ -0,0 +1,7 @@
+#ifndef PACKSYNC_H
+#define PACKSYNC_H
+
+// Sincroniza las recetas filtrando solo la carpeta packages/
+int fortune_sync_repository(const char *repo_url, const char *target_dir);
+
+#endif
